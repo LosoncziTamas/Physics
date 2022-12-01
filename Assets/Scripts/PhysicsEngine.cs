1a159ff7bbea2ed5e609a62128247165ad15b431
Tamás Losonczi <losonczi.tamas91@gmail.com>
2022-12-01 19:53:42 +0100

    [Gravity] gravity code WIP

--- Assets/Scripts/PhysicsEngine.cs
+++ Assets/Scripts/PhysicsEngine.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 
 public class PhysicsEngine : MonoBehaviour
 {
-    [field:SerializeField]
-    private Vector3 Velocity { get; [UsedImplicitly] set; }
-
+    private const float G = 6.674e-11f;  // 6.674×10−11 m3⋅kg−1⋅s−2
+    
+    [field:SerializeField] private Vector3 Velocity { get; [UsedImplicitly] set; }
     [field:SerializeField] public List<Vector3> ForcesInNewton { get; [UsedImplicitly] set; } = new();
     [field:SerializeField] public float MassInKilogram { get; [UsedImplicitly] set; }
+    
     [SerializeField] public bool _showTrails = true; 
     
     private LineRenderer _lineRenderer;
+    private List<PhysicsEngine> _physicsEngines;
 
     private void Start()
     {
+        _physicsEngines = new List<PhysicsEngine>(FindObjectsOfType<PhysicsEngine>());
         _lineRenderer = gameObject.AddComponent<LineRenderer>();
         _lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
         _lineRenderer.startColor = _lineRenderer.endColor = Color.yellow;
@@ -49,11 +52,32 @@ public class PhysicsEngine : MonoBehaviour
         var acceleration = totalForce / MassInKilogram;
         return acceleration * Time.fixedDeltaTime;
     }
+
+    private Vector3 CalculateGravityForFrame()
+    {
+        foreach (var engineA in _physicsEngines)
+        {
+            foreach (var engineB in _physicsEngines)
+            {
+                if (engineA == engineB)
+                {
+                    continue;
+                }
+                var massProduct = engineA.MassInKilogram * engineB.MassInKilogram;
+                var distance = Vector3.Distance(engineA.transform.position, engineB.transform.position);
+                var distanceSquare = distance * distance;
+                var gravity = G * massProduct / distanceSquare;
+                Debug.Log($"[Force on: {engineA.name} due to gravity of {engineB.name}]");
+            }
+        }
+        return Vector3.zero;
+    }
     
     private void FixedUpdate()
     {
         var force = CalculateForceForFrame();
         Velocity += CalculateAccelerationForFrame(force);
+        CalculateGravityForFrame();
         var delta = Velocity * Time.fixedDeltaTime;
         transform.position += delta;
     }
