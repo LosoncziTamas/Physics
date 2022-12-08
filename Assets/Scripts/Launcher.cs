f0f55f144e10585cec779bfd34b0395f6a145120
Tamás Losonczi <losonczi.tamas91@gmail.com>
2022-12-08 21:18:23 +0100

    [Gravity] add sounds and implement basic launch

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -1,16 +1,41 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class Launcher : MonoBehaviour
 {
     [SerializeField] private PhysicsEngine _ballPrefab;
-    
-    private void Launch()
+    [SerializeField] private float _maxLaunchSpeed;
+    [SerializeField] private float _speedOfIncrease;
+    [SerializeField] private AudioSource _windUp;
+    [SerializeField] private AudioSource _launch;
+
+    private bool _beingPushed;
+
+    private IEnumerator OnMouseDown()
     {
-        
+        _beingPushed = true;
+        var launchSpeed = 0f;
+        _windUp.Play();
+        while (_beingPushed)
+        {
+            yield return null;
+            var newSpeed = launchSpeed + Time.deltaTime * _speedOfIncrease;
+            launchSpeed = Math.Min(newSpeed, _maxLaunchSpeed);
+        }
+        _windUp.Stop();
+        Launch(launchSpeed);
+    }
+    
+    private void OnMouseUp()
+    {
+        _beingPushed = false;
     }
 
-    private void OnMouseDown()
+    private void Launch(float launchSpeed)
     {
-        Launch();
+        _launch.Play();
+        var ball = Instantiate(_ballPrefab);
+        ball.Velocity = new Vector3(-1, 1, 0).normalized * launchSpeed;
     }
 }
