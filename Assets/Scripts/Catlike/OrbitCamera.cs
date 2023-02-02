a872fab5728050d8881d3cf68fa6e9299348d513
Tamás Losonczi <losonczi.tamas91@gmail.com>
2023-02-02 21:51:11 +0100

    [Orbit Camera] add focus centering

--- Assets/Scripts/Catlike/OrbitCamera.cs
+++ Assets/Scripts/Catlike/OrbitCamera.cs
@@ -7,20 +7,54 @@ namespace Catlike
     {
         [SerializeField] private Transform _focus;
         [SerializeField, Range(1f, 20f)] private float _distance = 5f;
+        [SerializeField, Range(0f, 1f)] private float _focusCentering = 0.5f;
         [SerializeField, Min(0f)] private float _focusRadius = 1f;
 
+        private Vector3 _focusPoint;
         private Transform _cachedTransform;
 
         private void Start()
         {
+            _focusPoint = _focus.position;
             _cachedTransform = transform;
         }
 
         private void LateUpdate()
         {
-            var focusPoint = _focus.position;
+            UpdateFocusPoint();
             var lookDirection = _cachedTransform.forward;
-            _cachedTransform.localPosition = focusPoint - lookDirection * _distance;
+            // Align to look at focus point from given distance
+            _cachedTransform.localPosition = _focusPoint - lookDirection * _distance;
+        }
+
+        private void UpdateFocusPoint()
+        {
+            var targetPoint = _focus.position;
+            // Relaxed focus enabled
+            if (_focusRadius > 0)
+            {
+                var distance = Vector3.Distance(targetPoint, _focusPoint);
+                var t = 1.0f;
+                // Focus centering enabled
+                if (distance > 0.01f && _focusCentering > 0)
+                {
+                    // Smooth centering
+                    // ex.: 0.5 ^ 0.016 = 0.988
+                    t = Mathf.Pow(1f - _focusCentering, Time.unscaledDeltaTime);
+                }
+                // We are above focus range
+                if (distance > _focusRadius)
+                {
+                    // ex.: Min(0.988, 0.5)
+                    t = Mathf.Min(t, _focusRadius / distance);
+                }
+                // Lerp focus point from target to focus.
+                _focusPoint = Vector3.Lerp(targetPoint, _focusPoint, t);
+            }
+            else
+            {
+                _focusPoint = targetPoint;
+            }
         }
     }
 }
