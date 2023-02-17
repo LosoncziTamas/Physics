8b847bcf7002c1597e4864b04b78e63ceeafb16f
Tamás Losonczi <losonczi.tamas91@gmail.com>
2023-02-17 18:23:35 +0100

    [Orbit Camera] add orbit angles

--- Assets/Scripts/Catlike/OrbitCamera.cs
+++ Assets/Scripts/Catlike/OrbitCamera.cs
@@ -9,9 +9,11 @@ namespace Catlike
         [SerializeField, Range(1f, 20f)] private float _distance = 5f;
         [SerializeField, Range(0f, 1f)] private float _focusCentering = 0.5f;
         [SerializeField, Min(0f)] private float _focusRadius = 1f;
+        [SerializeField, Range(1f, 360f)] private float _rotationSpeedInDegreesPerSecond = 90f;
 
         private Vector3 _focusPoint;
         private Transform _cachedTransform;
+        private Vector2 _orbitAngles = new Vector2(45f, 0f);
 
         private void Start()
         {
@@ -22,9 +24,22 @@ namespace Catlike
         private void LateUpdate()
         {
             UpdateFocusPoint();
-            var lookDirection = _cachedTransform.forward;
+            var lookRotation = Quaternion.Euler(_orbitAngles);
+            var lookDirection = lookRotation * Vector3.forward;
+            var lookPosition = _focusPoint - lookDirection * _distance;;
             // Align to look at focus point from given distance
-            _cachedTransform.localPosition = _focusPoint - lookDirection * _distance;
+            _cachedTransform.SetPositionAndRotation(lookPosition, lookRotation);
+        }
+        
+        private void ManualRotation () {
+            var input = new Vector2(
+                Input.GetAxis("Vertical Camera"),
+                Input.GetAxis("Horizontal Camera")
+            );
+            const float e = 0.001f;
+            if (input.x is < -e or > e || input.y is < -e or > e) {
+                _orbitAngles += _rotationSpeedInDegreesPerSecond * Time.unscaledDeltaTime * input;
+            }
         }
 
         private void UpdateFocusPoint()
