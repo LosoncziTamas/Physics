89eadd8888fa79e854f116114cb48bff437ed0eb
Tamás Losonczi <losonczi.tamas91@gmail.com>
2023-03-05 17:41:15 +0100

    [Complex Gravity] add support for multiple gravity sources

--- Assets/Scripts/Catlike/CustomGravity.cs
+++ Assets/Scripts/Catlike/CustomGravity.cs
@@ -1,25 +1,45 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Catlike
 {
     public static class CustomGravity
     {
+        private static List<GravitySource> _sources = new();
+
+        public static void Register(GravitySource source)
+        {
+            Debug.Assert(!_sources.Contains(source), "Duplicated registration of gravity source!", source);
+            _sources.Add(source);
+        }
+
+        public static void Unregister(GravitySource source)
+        {
+            Debug.Assert(_sources.Contains(source), "Unregistration of unknown gravity source!", source);
+            _sources.Remove(source);
+        }
+        
         public static Vector3 GetGravity(Vector3 position)
         {
-            return position.normalized * Physics.gravity.y;
+            var summedGravity = Vector3.zero;
+            foreach (var gravitySource in _sources)
+            {
+                summedGravity += gravitySource.GetGravity(position);
+            }
+            return summedGravity;
         }
 
         public static Vector3 GetUpAxis(Vector3 position)
         {
-            // Assuming world origin is the center of our gravity source.
-            return position.normalized;
+            var g = GetGravity(position);
+            return -g.normalized;
         }
 
         public static Vector3 GetGravity(Vector3 position, out Vector3 upAxis)
         {
-            upAxis = GetUpAxis(position);
-            return GetGravity(position);
+            var g = GetGravity(position);
+            upAxis = -g.normalized;
+            return g;
         }
-        
     }
 }
---
+++ Assets/Scripts/Catlike/GravityPlane.cs
@@ -0,0 +1,41 @@
+using System;
+using UnityEngine;
+
+namespace Catlike
+{
+    public class GravityPlane : GravitySource
+    {
+        [SerializeField] private float _gravity = 9.81f;
+        [SerializeField, Min(0.0f)] private float _range = 1.0f;
+
+
+        public override Vector3 GetGravity(Vector3 position)
+        {
+            var trans = transform;
+            var up = trans.up;
+            var distance = Vector3.Dot(up, position - trans.position);
+            if (distance > _range)
+            {
+                return Vector3.zero;
+            }
+            return -_gravity * up;
+        }
+
+        private void OnDrawGizmos()
+        {
+            var trans = transform;
+            var scale = trans.localScale;
+            // Use the range to draw the gizmo above instead of the object's scale.
+            scale.y = _range;
+            Gizmos.matrix = Matrix4x4.TRS(trans.position, trans.rotation, scale);
+            var size = new Vector3(1, 0, 1);
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireCube(Vector3.zero, size);
+            if (_range > 0.0f)
+            {
+                Gizmos.color = Color.cyan;
+                Gizmos.DrawWireCube(Vector3.up, size);
+            }
+        }
+    }
+}
