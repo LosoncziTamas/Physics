520b82a47af79d68f005e6f6bbc971a787ae0fac
Tamás Losonczi <losonczi.tamas91@gmail.com>
2023-04-09 12:48:13 +0200

    [Reactive Environment] add acceleration zone

---
+++ Assets/Scripts/Catlike/AccelerationZone.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+namespace Catlike
+{
+    [RequireComponent(typeof(Collider))]
+    public class AccelerationZone : MonoBehaviour
+    {
+        [SerializeField, Min(0f)] private float _speed = 10.0f;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            var rb = other.attachedRigidbody;
+            if (rb)
+            {
+                Accelerate(rb);
+            }
+        }
+
+        private void Accelerate(Rigidbody rigidbody)
+        {
+            var velocity = rigidbody.velocity;
+            if (velocity.y > _speed)
+            {
+                return;
+            }
+            velocity.y = _speed;
+            rigidbody.velocity = velocity;
+        }
+    }
+}
